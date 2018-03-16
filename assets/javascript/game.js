7768999b3a2b333cbae00ddea8a70c2681ddf261
Lan Ejiri <lan.ejiri@yahoo.com>
2018-03-16 13:03:14 -0400

    got the base html

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+var crystal1Points = Math.floor(Math.random() *  12 + 1);
+var crystal2Points = Math.floor(Math.random() *  12 + 1);
+var crystal3Points = Math.floor(Math.random() *  12 + 1);
+var crystal4Points = Math.floor(Math.random() *  12 + 1);
+var goalScore = Math.floor(Math.random() *  120 + 19);
+var wins = 0;
+var losses = 0;
+
+
+$("#crystal1").click(function(){
+
+
+    // Clicking the button triggers an alert message.
+    alert("You clicked a crystal!");
+
+  });
+
+  console.log("is this working");
